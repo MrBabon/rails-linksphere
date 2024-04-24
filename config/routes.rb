6cef3c42550feb0d7a74cb880ffcfe7efd917d2a
MrBabon <christophedanna06@gmail.com>
2024-04-24 15:49:44 +0200

    setup our own versions of sessions an registration controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controller: {
+    sessions: 'users/sessions',
+    registration: 'users/registrations'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
