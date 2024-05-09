4131ae0b3f393594497fcf7cc9c49770199a43e9
MrBabon <christophedanna06@gmail.com>
2024-05-09 09:00:47 +0200

    cors modifie

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,20 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http:localhost:3000"
 
     resource "*",
       headers: :any,
       expose: ["Authorization"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  # allow do
+  #   origins "http:localhost:3000"
+
+  #   resource "*",
+  #     headers: :any,
+  #     expose: ["Authorization"],
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 end
