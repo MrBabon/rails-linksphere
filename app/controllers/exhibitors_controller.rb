9d8ee52b676396e5109039054732507f9033a023
MrBabon <christophedanna06@gmail.com>
2024-05-14 23:41:16 +0200

    show exhibitor controller ok

--- app/controllers/exhibitors_controller.rb
+++ app/controllers/exhibitors_controller.rb
@@ -1,2 +1,15 @@
 class ExhibitorsController < ApplicationController
+
+    def show
+        @exhibitor = Exhibitor.find(params[:id])
+        authorize @exhibitor
+
+        if @exhibitor.entreprise
+            render json: EntrepriseSerializer.new(@exhibitor.entreprise).serializable_hash
+        else
+            render json: { error: "Entreprise details not found for this exhibitor." }, status: :not_found
+        end
+    rescue ActiveRecord::RecordNotFound
+        render json: { error: "Exhibitor not found." }, status: :not_found
+    end
 end
--- app/policies/exhibitor_policy.rb
+++ app/policies/exhibitor_policy.rb
@@ -5,4 +5,8 @@ class ExhibitorPolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def show?
+    true
+  end
 end
